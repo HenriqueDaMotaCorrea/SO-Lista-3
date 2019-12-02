66c3bf2488552c07730ae6e57b45ab559d14e2dc
Henrique Mota <riquelhama@gmail.com>
2019-12-02 00:26:18 +0000

    Adiciona funções utilizadas nas threads

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
+// Lista 3 - Ex 1
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <pthread.h>
+#include <unistd.h>
+
+int segundos(){
+  return (rand() % 16) + 5;
+}
+
+int inputN(){
+  int n = 0;
+  printf("Número de threads: ");
+  scanf("%d", &n);
+  return n;
+}
+
+void *rotinaThread(){
+  int t = segundos();
+  printf("Vai dormir por %d segundos\n", t);
+  sleep(t);
+  printf("Bom dia mundo cruel!");
+  return NULL;
+}
 
 int main(void) {
-  printf("Hello World\n");
+  pthread_t thID;
+  int numThreads = inputN();
+  printf("Criando %d threads...\n", numThreads);
+  pthread_create(&thID, NULL, (void*)rotinaThread(), NULL);
+  printf("Nova thread criada: %ld\n", thID);
+  pthread_join(thID, NULL);
+  printf("Todas as threads terminaram\n");
   return 0;
 }
